21eaacb3232da913ac823dd23de3c27b9079a194
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2025-10-03 20:22:16 -0600

    Update compile SDK and target verison to Android 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.tenmilelabs.chefai"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.tenmilelabs.chefai"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
