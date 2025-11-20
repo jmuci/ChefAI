17a2706db9e902bef9282239c0f48f2a83249841
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2025-11-20 08:20:42 -0700

    Minor fixes

--- app/src/main/java/com/tenmilelabs/chefai/ui/createrecipe/components/StepCard.kt
+++ app/src/main/java/com/tenmilelabs/chefai/ui/createrecipe/components/StepCard.kt
@@ -1,4 +1,4 @@
-package com.tenmilelabs.chefai.ui.createrecipe
+package com.tenmilelabs.chefai.ui.createrecipe.components
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
