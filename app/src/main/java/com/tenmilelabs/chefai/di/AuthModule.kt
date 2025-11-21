f48f7949c1be9e180ba10d230feaf96890e32257
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2025-11-21 14:27:43 -0700

    Add Authentication (WIP)

---
+++ app/src/main/java/com/tenmilelabs/chefai/di/AuthModule.kt
@@ -0,0 +1,16 @@
+package com.tenmilelabs.chefai.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+/**
+ * Dagger Hilt module for providing authentication-related dependencies.
+ *
+ * Note: ApplicationScope and its CoroutineScope provider are defined in CoroutinesModule.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+    // Authentication-related providers can be added here if needed in the future
+}
---
+++ app/src/main/java/com/tenmilelabs/chefai/ui/auth/AuthState.kt
@@ -0,0 +1,42 @@
+package com.tenmilelabs.chefai.ui.auth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.collectAsState
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import com.tenmilelabs.chefai.domain.auth.SessionManager
+import com.tenmilelabs.chefai.domain.model.User
+import com.tenmilelabs.chefai.domain.model.UserSession
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+/**
+ * ViewModel that exposes the current user session state to Composables.
+ * This allows any screen to observe the authentication state.
+ */
+@HiltViewModel
+class AuthStateViewModel @Inject constructor(
+    private val sessionManager: SessionManager
+) : ViewModel() {
+    val userSession = sessionManager.userSession
+}
+
+/**
+ * Composable helper to easily access the current user session state.
+ * Usage:
+ * ```
+ * val userSession by rememberUserSession()
+ * when (userSession) {
+ *     is UserSession.Authenticated -> { /* Show authenticated content */ }
+ *     is UserSession.Unauthenticated -> { /* Show login screen */ }
+ *     is UserSession.Loading -> { /* Show loading indicator */ }
+ * }
+ * ```
+ */
+@Composable
+fun rememberUserSession(
+    viewModel: AuthStateViewModel = hiltViewModel()
+): State<UserSession> {
+    return viewModel.userSession.collectAsState()
+}
