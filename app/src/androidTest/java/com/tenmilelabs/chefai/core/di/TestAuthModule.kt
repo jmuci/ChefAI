29974e68b633d45ec51142439881643f47bbcdef
Jose M Mucientes <jm.mucientes.fayos@gmail.com>
2026-02-24 21:05:07 -0700

    Bind transactionRunner for E2E tests too

--- app/src/androidTest/java/com/tenmilelabs/chefai/core/di/TestAuthModule.kt
+++ app/src/androidTest/java/com/tenmilelabs/chefai/core/di/TestAuthModule.kt
@@ -6,6 +6,8 @@ import com.tenmilelabs.chefai.auth.data.network.AuthNetworkDataSource
 import com.tenmilelabs.chefai.auth.data.network.FakeAuthNetworkDataSource
 import com.tenmilelabs.chefai.auth.domain.SessionManager
 import com.tenmilelabs.chefai.auth.domain.TokenProvider
+import com.tenmilelabs.chefai.core.data.local.room.RoomTransactionRunner
+import com.tenmilelabs.chefai.core.data.local.room.TransactionRunner
 import com.tenmilelabs.chefai.core.data.repository.DefaultMetadataRepository
 import com.tenmilelabs.chefai.core.domain.repository.MetadataRepository
 import com.tenmilelabs.chefai.recipes.data.repository.DefaultRecipeRepository
@@ -73,6 +75,8 @@ abstract class TestRepositoryModule {
             return FakeSecurePreferences()
         }
     }
+    @Binds
+    abstract fun bindTransactionRunner(impl: RoomTransactionRunner): TransactionRunner
     
     /**
      * Binds the recipe repository (same as production).
